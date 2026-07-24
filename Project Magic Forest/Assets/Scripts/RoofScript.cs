31142269b5428848fdf7c9ce18f98b29afe32fff
joalnu10 <joalnu10@gmail.com>
2026-07-24 10:47:13 -0400

    AA
    AA

--- Project Magic Forest/Assets/Scripts/RoofScript.cs
+++ Project Magic Forest/Assets/Scripts/RoofScript.cs
@@ -3,16 +3,29 @@ using UnityEngine;
 public class RoofScript : MonoBehaviour
 {
     SpriteRenderer sr_roof;
-    BoxCollider2D bc_roof;
+    BoxCollider2D[] bc_roof;
     public GameObject roof;
     public GameObject door;
-    
+   
 
     void OnTriggerEnter2D(Collider2D other)
     {
+
        sr_roof = roof.GetComponent<SpriteRenderer>();
        sr_roof.enabled = false;
-       bc_roof = roof.GetComponent<BoxCollider2D>();
-       bc_roof.enabled = false;
+       bc_roof = roof.GetComponents<BoxCollider2D>();
+       bc_roof[0].enabled = false;
+       bc_roof = roof.GetComponents<BoxCollider2D>();
+       bc_roof[1].enabled = false;
+       bc_roof = roof.GetComponents<BoxCollider2D>();
+       bc_roof[2].enabled = false;
+       bc_roof = roof.GetComponents<BoxCollider2D>();
+       bc_roof[3].enabled = false;
+       Debug.Log("1");
+       
+    }
+    public void OnAnimationEnd()
+    {
+        
     }
 }
