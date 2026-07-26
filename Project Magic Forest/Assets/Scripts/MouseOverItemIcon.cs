5147a56b3a39d4f281e53d1114458848d25245ab
joalnu10 <joalnu10@gmail.com>
2026-07-26 05:12:03 -0400

    8th Grader fixed the tooltip
    tooltip fix

--- Project Magic Forest/Assets/Scripts/MouseOverItemIcon.cs
+++ Project Magic Forest/Assets/Scripts/MouseOverItemIcon.cs
@@ -1,27 +1,62 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using TMPro;
 
 public class MouseOverItemIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Vector3 originalScale;
-
     [SerializeField] private GameObject iconTooltip;
+    private RectTransform rectTransform;
+    private bool isHovering;
 
-    void Update()
+    private void Awake()
     {
-       if() 
-    }
-   
-  
-     public void OnPointerEnter(PointerEventData eventData)
-    {
-       iconTooltip.SetActive(true);
+        rectTransform = GetComponent<RectTransform>();
     }
 
-    // Triggered when mouse leaves the UI element
+    private void Update()
+    {
+        if (!isHovering || iconTooltip == null || rectTransform == null)
+        {
+            return;
+        }
+
+        if (!RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition, null))
+        {
+            HideTooltip();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ShowTooltip();
+    }
+
     public void OnPointerExit(PointerEventData eventData)
     {
-        iconTooltip.SetActive(false);
+        HideTooltip();
+    }
+
+    private void OnDisable()
+    {
+        HideTooltip();
+    }
+
+    private void ShowTooltip()
+    {
+        isHovering = true;
+
+        if (iconTooltip != null)
+        {
+            iconTooltip.SetActive(true);
+        }
+    }
+
+    private void HideTooltip()
+    {
+        isHovering = false;
+
+        if (iconTooltip != null)
+        {
+            iconTooltip.SetActive(false);
+        }
     }
 }
