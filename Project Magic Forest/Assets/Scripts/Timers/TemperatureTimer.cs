68535ae570ae235727932df0a26cea6a934ee799
joalnu10 <joalnu10@gmail.com>
2026-07-26 12:30:01 -0400

    Time to go

--- Project Magic Forest/Assets/Scripts/Timers/TemperatureTimer.cs
+++ Project Magic Forest/Assets/Scripts/Timers/TemperatureTimer.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TemperatureTimer : Timer
 {
     [Header("Temperature Timer")]
     [SerializeField] private GameObject playerToKill;
+    public GameObject SnowIn;
 
     public override void OnTimerEnd()
+    {
+        SnowIn.SetActive(true);
+        Invoke("LoadTitleScreen", 2f);
+
+        
+
+        Debug.LogWarning("[TemperatureTimer] SceneFlowManager not found. Could not load title screen.");
+    }
+    public void LoadTitleScreen()
     {
         SceneFlowManager sceneFlowManager = SceneFlowManager.Instance ?? FindFirstObjectByType<SceneFlowManager>();
         if (sceneFlowManager != null)
         {
-            sceneFlowManager.LoadScene("Main Menu");
+            
+           
+
+
+            sceneFlowManager.LoadStartScene();
             return;
         }
-
-        Debug.LogWarning("[TemperatureTimer] SceneFlowManager not found. Could not load title screen.");
     }
 }
